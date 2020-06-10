c6777de2fa692d1ededd7995a3d2f4ffad8564f6
Jack Li <38816464+lijaack@users.noreply.github.com>
2020-06-09 23:55:20 -0700

    Update flashcardData.js

--- src/flashcard-data/flashcardData.js
+++ src/flashcard-data/flashcardData.js
@@ -6,7 +6,7 @@ const java = [
     {
         question:"What does it mean to be a “pass-by-value” language? How is this different from passing by reference? ",
         answer:"PBV is copying the value to a new variable and passing that to a method as a parameter. PBR is passing a reference to the actual value as a parameter."
-    }
+    },
     {
         question:"What is the difference between the JRE, JDK, and JVM? ",
         answer: "JRE(Java Runtime Environment) is a container for class libraries, class loaders and the JVM. JVM(Java Virtual Machine) is used to execute Java programs on your system. JDK(Java Development Kit) uses the JRE and contains developer tools such as the compiler to allow developers to create Java programs to be run by the JVM and JRE. "
