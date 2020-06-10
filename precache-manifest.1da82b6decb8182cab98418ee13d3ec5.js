b5ca35cebb6926d2235436f1400312ace0304346
Jack Li <38816464+lijaack@users.noreply.github.com>
2020-06-09 23:40:21 -0700

    Updates

---
+++ precache-manifest.1da82b6decb8182cab98418ee13d3ec5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e971aeb5612b1f2e61c88ff4fcdbd7db",
+    "url": "/flashcards/index.html"
+  },
+  {
+    "revision": "7de68a4a70a87e0a906c",
+    "url": "/flashcards/static/css/main.58fd5152.chunk.css"
+  },
+  {
+    "revision": "8f0e2c1aaf9042005767",
+    "url": "/flashcards/static/js/2.9ef7b8dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/flashcards/static/js/2.9ef7b8dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7de68a4a70a87e0a906c",
+    "url": "/flashcards/static/js/main.86e72de7.chunk.js"
+  },
+  {
+    "revision": "c9bf98cac3956cbb5b7b",
+    "url": "/flashcards/static/js/runtime-main.1952d549.js"
+  }
+]);
