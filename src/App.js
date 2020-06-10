284672c723fa7932496cf6fb7fb360d96ecda57a
Jack Li <38816464+lijaack@users.noreply.github.com>
2020-06-09 23:16:33 -0700

    z

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import flashcardData from './flashcard-data/flashcardData';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state={
+    topic:'',
+    questions:{},
+    random: null,
+    showAnswer:false
+  }
+  componentDidMount(){
+    this.setState({questions: flashcardData});
+  }
+  onChange  = event => {
+    this.setState({ [event.target.name]: event.target.value }, this.generateRandom);
+  }
+  generateRandom=()=>{
+    this.setState({random: Math.floor(Math.random() * this.state.questions[this.state.topic].length)})
+    this.setState({showAnswer: false});
+  }
+  showAnswer = () =>{
+    this.setState({showAnswer: true});
+  }
+  render() {
+    let question
+    let answer
+
+    if(this.state.random !=null){
+      question = <h3>{this.state.questions[this.state.topic][this.state.random].question}</h3>;
+      answer = <p>{this.state.questions[this.state.topic][this.state.random].answer}</p>;
+    }
+    return (
+          <div className="App">
+            <div className="container">
+
+              <div className="row ">
+                <div className="col-md-4 ">
+                </div>
+                <div className="col-md-4 ">
+                  <div className="form-group">
+                    <label htmlFor="topic">choose a topic</label>
+                    <select className="form-control form-control-sm dropdown"  defaultValue={'DEFAULT'} onChange={this.onChange} name="topic" id="topic">
+                      <option value="DEFAULT" disabled>Choose a Topic</option>
+                      { Object.keys(this.state.questions).map((k, i)=>{
+                        return <option key={i} value={k}>{k}</option>
+                      })}
+                    </select>
+                  </div>
+                </div>
+                <div className="col-md-4 ">
+                </div>      
+              </div>
+
+              <div className="row">
+                <div className="col-md-2 ">
+                </div>
+                <div className="col-md-6">
+                    {question}
+                    {this.state.showAnswer? answer:''}
+                    <button className="btn btn-primary" onClick={this.showAnswer}>showAnswer</button>
+                    <button className="btn btn-info" onClick={this.generateRandom}>New Question</button>
+                </div>
+                <div className="col-md-2 ">
+                </div>      
+
+              </div>
+            </div>
+          </div>
+    );
+  } 
 }
 
 export default App;
---
+++ src/flashcard-data/flashcardData.js
@@ -0,0 +1,27 @@
+const java = [
+    {
+        question: "h2222222i",
+        answer:"1"
+    },
+    {
+        question:"hi2",
+        answer:"3"
+    }
+]
+const testAutomation = [
+    {
+        question: "h324i",
+        answer:"1"
+    },
+    {
+        question:"hi2",
+        answer:"3"
+    }
+]
+const flashcardData = {
+    java:java,
+    testAutomation:testAutomation
+};
+
+
+export default flashcardData;
